a87d26b989f6dccf5d7e4f304adfe991a73ddcf2
Jack Brookes <jbrookes187@gmail.com>
2017-10-15 23:55:12 +0100

    fixed namespaces

---
+++ Assets/ExpMngr/Scripts/Form/DropDownController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ExpMngr
+{
+    public class DropDownController : FormElementController
+    {
+        public Dropdown dropdown;
+        protected override void Setup()
+        {
+            dropdown.ClearOptions();
+            dropdown.AddOptions(entry.dropDownValues);
+        }
+
+        public override object GetContents()
+        {
+            return entry.dropDownValues[dropdown.value];
+        }
+
+    }
+}
