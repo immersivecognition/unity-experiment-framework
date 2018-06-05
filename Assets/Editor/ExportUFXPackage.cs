b32099f80256fc5a65a42d10de5ad5e59ef5beb6
jackbrookes <jbrookes187@gmail.com>
2018-06-05 20:47:10 +0100

    Added exporting function

---
+++ Assets/Editor/ExportUFXPackage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class ExportUFXPackage : MonoBehaviour {
+
+    // Add a menu item named "Do Something" to MyMenu in the menu bar.
+    [MenuItem("UXF/Export package")]
+    static void ExportPackage()
+    {
+		string[] assets = new string[]
+		{
+			"Assets/Plugins",
+            "Assets/StreamingAssets",
+            "Assets/UXF"
+		};
+
+
+
+		if (!Directory.Exists("Package"))
+			Directory.CreateDirectory("Package");
+		string path = "Package/UXF.unitypackage";
+
+
+        ExportPackageOptions options = ExportPackageOptions.Recurse;
+
+        AssetDatabase.ExportPackage(assets, path, options);
+    }
+}
