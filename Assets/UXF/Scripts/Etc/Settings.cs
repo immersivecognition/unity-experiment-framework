a16b86c9cb8cd5338661859a296ae00520fa3b25
Jack Brookes <jbrookes187@gmail.com>
2020-09-24 23:43:28 +0100

    settings default values

--- Assets/UXF/Scripts/Etc/Settings.cs
+++ Assets/UXF/Scripts/Etc/Settings.cs
@@ -242,6 +242,146 @@ namespace UXF
             return (List<object>) Get(key);
         }
 
+
+        /// <summary>
+        /// Get a boolean setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public bool GetBool(string key, bool valueIfNotFound) { return ContainsKey(key) ? GetBool(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a integer setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public int GetInt(string key, int valueIfNotFound) { return ContainsKey(key) ? GetInt(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a float setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public float GetFloat(string key, float valueIfNotFound) { return ContainsKey(key) ? GetFloat(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a long setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public long GetLong(string key, long valueIfNotFound) { return ContainsKey(key) ? GetLong(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a double setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public double GetDouble(string key, double valueIfNotFound) { return ContainsKey(key) ? GetDouble(key) : valueIfNotFound; } 
+
+        /// <summary>
+        /// Get a string setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public string GetString(string key, string valueIfNotFound) { return ContainsKey(key) ? GetString(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a dictionary setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public Dictionary<string, object> GetDict(string key, Dictionary<string, object> valueIfNotFound) { return ContainsKey(key) ? GetDict(key) : valueIfNotFound;  }
+
+        /// <summary>
+        /// Get a object setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public object GetObject(string key, object valueIfNotFound) { return ContainsKey(key) ? GetObject(key) : valueIfNotFound;  }
+
+        /// <summary>
+        /// Get a boolean list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found. Returns valueIfNotFound if the key is not found in the settings.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<bool> GetBoolList(string key, List<bool> valueIfNotFound) { return ContainsKey(key) ? GetBoolList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a integer list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<int> GetIntList(string key, List<int> valueIfNotFound) { return ContainsKey(key) ? GetIntList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a float list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<float> GetFloatList(string key, List<float> valueIfNotFound) { return ContainsKey(key) ? GetFloatList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a long list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<long> GetLongList(string key, List<long> valueIfNotFound) { return ContainsKey(key) ? GetLongList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a double list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<double> GetDoubleList(string key, List<double> valueIfNotFound) { return ContainsKey(key) ? GetDoubleList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a string list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<string> GetStringList(string key, List<string> valueIfNotFound) { return ContainsKey(key) ? GetStringList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a Dictionary<string, object> list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// If the setting references a setting stored in the settings json file, a copy of the list will be returned. If it is a setting created with settings.SetValue(...), the original reference will be returned. 
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<Dictionary<string, object>> GetDictList(string key, List<Dictionary<string, object>> valueIfNotFound) { return ContainsKey(key) ? GetDictList(key) : valueIfNotFound; }
+
+        /// <summary>
+        /// Get a object list setting value. If it is not found, the request will cascade upwards in each parent setting until one is found.
+        /// </summary>
+        /// <param name="key">The key (name) of the setting.</param>
+        /// <param name="valueIfNotFound">The value returned if the setting does not exist (i.e., a default value).</param>
+        public List<object> GetObjectList(string key, List<object> valueIfNotFound) { return ContainsKey(key) ? GetObjectList(key) : valueIfNotFound; }
+
+
+        public bool ContainsKey(string key)
+        {
+            try
+            {
+                return baseDict.ContainsKey(key);
+            }
+            catch (KeyNotFoundException)
+            {
+                if (parentSettingsContainer != null && parentSettingsContainer.settings != null)
+                {
+                    return parentSettingsContainer.settings.ContainsKey(key);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
         /// <summary>
         /// Set a setting value.
         /// </summary>
