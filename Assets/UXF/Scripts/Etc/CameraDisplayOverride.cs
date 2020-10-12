c09fd001761fbffa1555cab687af089a69344d1e
Jack Brookes <jbrookes187@gmail.com>
2020-10-12 10:21:18 +0100

    examples fix

---
+++ Assets/UXF/Scripts/Etc/CameraDisplayOverride.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UXF.EditorUtils
+{
+    /// <summary>
+    /// Class which handles the cascading settings system. Wraps a Dictionary.
+    /// </summary>
+    public class CameraDisplayOverride : MonoBehaviour
+    {
+        
+        /// <summary>
+        /// Called when the script is loaded or a value is changed in the
+        /// inspector (Called in the editor only).
+        /// </summary>
+        void OnValidate()
+        {
+            GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.None;
+        }
+
+    }
+}
