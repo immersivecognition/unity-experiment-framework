6b79e8837844694ca951c20d4bcc73a264c36022
Jörn Alexander Quent <alexander.quent@rub.de>
2022-09-07 13:03:13 +0800

    Update ScreenRayTracker.cs

--- Assets/UXF/Scripts/Trackers/ScreenRayTracker.cs
+++ Assets/UXF/Scripts/Trackers/ScreenRayTracker.cs
@@ -7,16 +7,14 @@ using System.Linq;
 /// <summary>
 /// Attach this component to any gameobject (e.g. an empty one) and assign it in the trackedObjects field in an ExperimentSession to record
 /// if ray casted from camera is hitting anything. NOTE: Update Type must be set to MANUAL. 
+/// Please provide coordinates in form of lists named ray_x & ray_y in your .json file. For example \n\"ray_x\": [0.5],\n\"ray_y\": [0.5]\nif you want to have one ray in the middle of the screen. Multiple rays can be provide with this method."
 /// </summary>
-public class ScreenRayTracker : Tracker {
+public class ScreenRayTracker : Tracker 
+{
 	// Public vars
     [Header("Necessary Input")]
     public Camera cam;
     public Session session;
-
-    [Header("Ray coordinates")]
-    [TextArea(10, 10)]
-    public string HowToUse = "Please provide coordinates in form of lists named ray_x & ray_y in your .json file. For example \n\"ray_x\": [0.5],\n\"ray_y\": [0.5]\nif you want to have one ray in the middle of the screen. Multiple rays can be provide with this method.";
 
     [Header("Optional Input")]
     [Tooltip("Enable if you want to visualise the rays in the scene view and the console output.")]
@@ -42,9 +40,11 @@ public class ScreenRayTracker : Tracker {
     // Start calc
     void Start(){
         // Create layer mask 
-        if(useLayerMask){
+        if(useLayerMask)
+        {
             layerMask = LayerMask.GetMask(layerMaskNames);
-        } else {
+        } else 
+        {
             layerMask = ~0; // Set to everything as no mask is wanted. 
         }
 
@@ -55,7 +55,8 @@ public class ScreenRayTracker : Tracker {
     /// <summary>
     /// Gets coordinates for the rays in screen space from .json file and prints screen resolution
     /// </summary>
-    public void GetRayCoordinates(){
+    public void GetRayCoordinates()
+    {
         // Coordinates
         x = session.settings.GetFloatList("ray_x");
         y = session.settings.GetFloatList("ray_y");
@@ -67,24 +68,28 @@ public class ScreenRayTracker : Tracker {
     /// <summary>
     /// Starts the recording. This method needs to be added to [UXF_Rig] events called On Trial Begin
     /// </summary>
-    public void StartRecording(){
+    public void StartRecording()
+    {
     	recording = true;
     }
 
     /// <summary>
     /// Stops the recording. This method needs to be added to [UXF_Rig] events called On Trial End
     /// </summary>
-    public void StopRecording(){
+    public void StopRecording()
+    {
     	recording = false;
     }
 
-    IEnumerator RecordRoutine(){
+    IEnumerator RecordRoutine()
+    {
         while (true){
             if (recording){
-                objectDetected = ray2detectObjects(x, y, cam);
+                objectDetected = Ray2DetectObjects(x, y, cam);
                 for(int i = 0; i < numRays; i++){
                     // When no object was detected save only if saveNoObject is true
-                    if(objectDetected[i] != noObjectString){
+                    if(objectDetected[i] != noObjectString)
+                    {
                         var values = new UXFDataRow();
                         values.Add(("rayIndex", i));
                         values.Add(("x", x[i]));
@@ -105,17 +110,18 @@ public class ScreenRayTracker : Tracker {
     /// Set headers and measurment descriptor
     /// </summary>
     public override string MeasurementDescriptor => "ObjectsOnScreenTracker";
-    public override IEnumerable<string> CustomHeader => new string[] { "rayIndex", "x", "y", "objectDetected"};
+    public override IEnumerable<string> CustomHeader => new string[] {"rayIndex", "x", "y", "objectDetected"};
 
-    // Get values
-    protected override UXFDataRow GetCurrentValues(){
+    protected override UXFDataRow GetCurrentValues()
+    {
         return currentRow;
     }
 
     /// <summary>
-    /// Function to detect objects on screen by rays
+    /// Function that casts the rays and detects the hits. 
     /// </summary>
-    List<string> ray2detectObjects(List<float> x, List<float> y, Camera cam){
+    List<string> Ray2DetectObjects(List<float> x, List<float> y, Camera cam)
+    {
     	// Get number of rays
     	numRays = y.Count;
 
