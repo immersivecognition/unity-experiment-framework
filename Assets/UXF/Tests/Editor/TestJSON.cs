d5986787fff16deae0717d5218324904efb10cc7
Govert Buijs <buijs@rsm.nl>
2025-02-28 11:08:44 +0100

    Fix Tests for Linux/OS X

--- Assets/UXF/Tests/Editor/TestJSON.cs
+++ Assets/UXF/Tests/Editor/TestJSON.cs
@@ -1,28 +1,23 @@
 ﻿using UnityEngine;
-using UnityEditor;
-using UnityEngine.TestTools;
 using NUnit.Framework;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 
 namespace UXF.Tests
 {
-	public class TestJSON
-	{
+    public class TestJSON
+    {
+        Dictionary<string, object> dict = new Dictionary<string, object>()
+        {
+            {"string", "aaa"},
+            {"bool", true},
+            {"int", 3},
+            {"float", 3.14f},
+            {"list", new List<object>() {1, 2, 3, 4}},
+            {"array", new object[] {1, 2, 3, 4}},
+            {"object", new Dictionary<string, object>() {{"a", 1}, {"b", "abc"}}}
+        };
 
-		Dictionary<string, object> dict = new Dictionary<string, object>()
-		{
-			{"string", "aaa"},
-			{"bool", true},
-			{"int", 3},
-			{"float", 3.14f},
-			{"list", new List<object>(){1, 2, 3, 4}},
-			{"array", new object[]{1, 2, 3, 4}},
-			{"object", new Dictionary<string, object>(){{"a", 1}, {"b", "abc"}}}
-		};
-
-		const string jsonString = @"{
+        const string jsonString = @"{
     ""string"": ""aaa"",
     ""bool"": true,
     ""int"": 3,
@@ -46,22 +41,22 @@ namespace UXF.Tests
 }";
 
 
-		[Test]
-		public void JSONIndentation()
-		{
-			string outString = MiniJSON.Json.Serialize(dict);
-			Assert.AreEqual(jsonString, outString);
-		}
+        [Test]
+        public void JSONIndentation()
+        {
+            string outString = MiniJSON.Json.Serialize(dict);
+            Assert.AreEqual(jsonString, outString.Replace("\r", ""));
+        }
 
-		[Test]
-		public void VectorSerialize()
-		{
+        [Test]
+        public void VectorSerialize()
+        {
             var vectorDict = new Dictionary<string, object>
             {
-                { "vector3", new Vector3(1, 2, 3) },
-                { "vector2", new Vector2(1, 2) }
+                {"vector3", new Vector3(1, 2, 3)},
+                {"vector2", new Vector2(1, 2)}
             };
-			
+
             string vectorString = @"{
     ""vector3"": {
         ""x"": 1,
@@ -73,11 +68,9 @@ namespace UXF.Tests
         ""y"": 2
     }
 }";
-            
+
             string outString = MiniJSON.Json.Serialize(vectorDict);
-			Assert.AreEqual(vectorString, outString);
-		}
-
-	}
-
+            Assert.AreEqual(vectorString, outString.Replace("\r", ""));
+        }
+    }
 }
